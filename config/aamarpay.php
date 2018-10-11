166bf00c1154b88a4e043dca5e91be0c4aa70cb3
orbachinujbuk\Orbachin Ujbuk <orbachinujbuk@gmail.com>
2018-10-12 01:43:06 +0600

    aamarpay integration phase 4

--- config/aamarpay.php
+++ config/aamarpay.php
@@ -3,7 +3,7 @@
 return [
     'store_id' => 'duits',
     'signature_key' => '4170582cc27b67e484153f90131bcf05',
-    'sandbox' => true,
+    'sandbox' => false,
     'redirect_url' => [
         'success' => [
             'route' => 'payment.success' // payment.success
