b7b5c18635d5222902ba9d1a198efebe93a8ec30
orbachinujbuk\Orbachin Ujbuk <orbachinujbuk@gmail.com>
2018-10-12 02:43:19 +0600

    aamarpay integration phase 16

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,17 +13,15 @@ class PaymentController extends Controller
     public function paymentSuccessOrFailed(Request $request)
     {
         $registration_id = $request->get('opt_a');
-        $amount = $request->get('opt_b');
         if($request->get('pay_status') == 'Failed') {
-            Session::flash('info',$registration_id.': You need to make the payment! Pay Tk:'.$amount);
+            Session::flash('info',$registration_id.': You need to make the payment!');
             return redirect()->back();
         }
         
-        $amount = $request->get('opt_b');
-        $aamarpay = new Aamarpay();
-        $valid  = $aamarpay->valid($request, $amount);
+        $amount_request = $request->get('opt_b');
+        $amount_paid = $request->get('amount');
         
-        if($valid) {
+        if($amount_paid == $amount_request) {
           $registration = ITFestRegistration::where('registration_id', $registration_id);
           $registration->trxid = $request->get('mer_txnid');
           $registration->payment_status = 1;
